df0f3fceff825fcb8fa60a371e661a7945e14c9b
jrodr117 <jrodr113@msudenver.edu>
2022-09-25 12:17:28 -0600

    created a function to open, read csv data and save it in a list of lists called sales

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import csv
 
-with open("monthly_sales.csv") as data_file:
-    data = csv.reader(data_file)
-    for row in data:
-        print(row)
+
+def open_read_csv():
+    with open("monthly_sales.csv") as data_file:
+        data = csv.reader(data_file)
+        sales = []
+        for row in data:
+            sales.append([row[0], int(row[1])])
+        print(sales)
