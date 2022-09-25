2bec297a6cb72b9c89dfd5e653b9c024269066df
jrodr117 <jrodr113@msudenver.edu>
2022-09-25 12:46:45 -0600

    made a function that saves data to csv file

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def open_read_csv():
         for row in data:
             sales.append([row[0], int(row[1])])
         print(sales)
+
+
+def save_to_csv(data):
+    with open("monthly_sales.csv") as data_file:
+        writer = csv.writer(data_file)
+        for row in data:
+            writer.write([row[0], int(row[1])])
